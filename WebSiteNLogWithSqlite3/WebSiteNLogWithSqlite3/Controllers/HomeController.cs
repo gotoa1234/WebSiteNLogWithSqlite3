db432dfbbaf0a380b84f5f182ba7a84988d01e36
gotoa1234 <cap8825@gmail.com>
2017-09-07 00:58:56 +0800

    Initinal
    0020. Nlog + Sqlite3微型DB模組化Log記錄方式

---
+++ WebSiteNLogWithSqlite3/WebSiteNLogWithSqlite3/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteNLogWithSqlite3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                LogManager.GetLogger("Action Index").Info("好人一生平安");
+                //以下故意將型別設成Null 再塞值，造成Exception
+                List<object> testObject = new List<object>();
+                testObject = null;
+                testObject.Add(null);
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetLogger("Action Index").ErrorException("Title:測試發生錯誤", ex);
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebSiteNLogWithSqlite3/WebSiteNLogWithSqlite3/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebSiteNLogWithSqlite3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            SqliteDBInitinal();//當網站建立時會自動建立SqliteDB
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SqliteDBInitinal()
+        {
+            //建立DB
+            try
+            {
+                //Sqlite3DB的建立路徑 - Nlog的路徑
+                var nlogSqlitePath = HttpContext.Current.Server.MapPath("~/App_Data/NLog_Record.s3db");
+                //
+                SQLiteConnection.CreateFile(nlogSqlitePath);
+            }
+            catch (Exception ex)//當發生錯誤時
+            {
+                //使用Nlog內建的Error Exception紀錄
+                LogManager.GetLogger("MyNlog").ErrorException("建立.s3db時發生例外", ex);
+            }
+        }
+
+    }
+}
